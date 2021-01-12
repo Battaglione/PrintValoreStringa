559c50340888edb973039cff443effb869a5cf90
Azel04 <75199595+Azel04@users.noreply.github.com>
2021-01-12 10:32:51 +0100

    Aggiunto Output valore binario

--- PrintString.c
+++ PrintString.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #define N 80
 int main(){
-    char carattere[N];
-    int i;
+    char carattere[N], bin[N];
+    int i, j, temp;
 
     printf("Inserisci la stringa\n");
     gets(carattere);
@@ -12,11 +12,18 @@ int main(){
     printf("La stringa alla fine di questa frase vale: FRASE  >>> ");
     puts(carattere);
     for(i = 0; i < strlen(carattere); i++){
-        if(carattere[i] != 32){
-            printf("\n%c %d", carattere[i], (int)carattere[i]);
+        temp = (int)carattere[i];
+        for(j = 0; temp >= 1; j++){
+            bin[j] = temp % 2;
+            temp = temp / 2;
         }
-        else{
+        temp = j;
+        printf("\n%c %d ", carattere[i], (int)carattere[i]);
+        for(j = temp; j >= 0; j--){
+            printf("%d", bin[j]);
+        }
             printf("\n");
-        }
     }
+
+    system("pause");
 }
